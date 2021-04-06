2a4579ad3ea78e4f092217484f7e5b4a0735b144
convexlens <asam0707@daum.net>
2021-04-07 05:39:08 +0900

    자료형 연습

---
+++ practice_variable.py
@@ -0,0 +1,44 @@
+# print(5)
+# print(10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+
+
+# # 참 / 거짓
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+
+# 애완동물을 소개해 주세요
+# print("우리집 강아지는 이름은 연탄이예요")
+# print("연탄이는 4살이며, 산책을 아주 좋아해요")
+# print("연탄이는 어른일까요? True")
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집 " + animal + "는 이름은 " + name + "예요")
+#print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, " , hobby , "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
